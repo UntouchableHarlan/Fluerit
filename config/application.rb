367cb0bf7f7bf2817657a2e2905a74a0a9725266
Salomon <salomonpluviose@yahoo.com>
2016-10-26 22:45:54 -0400

    second shot at heroku iframe

--- config/application.rb
+++ config/application.rb
@@ -16,14 +16,13 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.action_dispatch.default_headers = {
-  'X-Frame-Options' => 'ALLOWALL'
-}
-
 module FlirtAlert
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
