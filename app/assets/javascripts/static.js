95b497ad69e2710d0ace1847e41abfc1f4cd85f9
Salomon <salomonpluviose@yahoo.com>
2016-10-27 15:32:20 -0400

    added a onloading function for better experience

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,11 +1,23 @@
+var hideSpinner = function(){
+  $('#spinner').hide();
+  $('.container').show();
+};
+
+var showSpinner = function(){
+  $("#spinner").show();
+  $('.container').hide();
+}
+
 $(document).ready(function() {
   $("#flirt").on('click', function(event) {
     event.preventDefault();
     $.ajax({
       url: '/search',
       method: 'GET',
+      onLoading: showSpinner(),
       success: function(response) {
         console.log(response);
+        hideSpinner();
         var pic = response.giphy.hash.embed_url;
         console.log(pic.split("http://")[1]);
         $("#quote").html(response.quote.quote);
